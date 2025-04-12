9e2ce345c4645009cde5f22cbe41d007a7b961bf
Ben McNicholl <git@benmcnicholl.com>
2025-04-12 20:17:55 +1000

    chore: formatted files

--- internal/commands/stdio.go
+++ internal/commands/stdio.go
@@ -7,11 +7,9 @@ import (
 	"github.com/wolfeidau/buildkite-mcp-server/internal/buildkite"
 )
 
-type StdioCmd struct {
-}
+type StdioCmd struct{}
 
 func (c *StdioCmd) Run(ctx context.Context, globals *Globals) error {
-
 	s := server.NewMCPServer(
 		"github-mcp-server",
 		globals.Version,
